e794cb2c9c5b58f7f79da9a9f19e64ef76e607f6
jp <jphamada@gmail.com>
2014-02-12 16:50:28 -0300

    musica
    inclusion de musica y teatro

--- www/js/index.js
+++ www/js/index.js
@@ -64,7 +64,36 @@ var app = {
     },
 
 
-    blog: function(){
+    musica: function(){
+        function getBlogs() {
+            var dfd = $.Deferred();
+            $.ajax({
+                url: 'http://www.jphamada.com.ar/api/get_recent_posts/',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data){
+                    var source   = $("#blog-template").html();
+                    var template = Handlebars.compile(source);
+                    var blogData = template(data);
+                    $('#blog-data').html(blogData);
+                    $('#blog-data').trigger('create');
+                    dfd.resolve(data);
+
+                },
+                error: function(data){
+                    console.log(data);
+                }
+            });
+            return dfd.promise();
+        };
+
+        getBlogs().then(function(data){
+            $('#all-posts').on('click','li', function(e){                
+                localStorage.setItem('postData', JSON.stringify(data.posts[$(this).index()]));
+            });
+        });
+
+    teatro: function(){
         function getBlogs() {
             var dfd = $.Deferred();
             $.ajax({
